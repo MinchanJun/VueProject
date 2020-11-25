1cc8c3b76e2ea7f568d7a557430709101a23dcd4
Minchan Jun <minchan.jun@capsim.com>
2020-11-25 15:42:50 -0500

    Simple Unit Testing Added

---
+++ tests/unit/Photographer.spec.js
@@ -0,0 +1,35 @@
+import { shallowMount } from '@vue/test-utils'
+import Photographer from '@/components/Photographer.vue'
+
+
+const factory = (values = {}) => {
+  return shallowMount(Photographer, {
+    propsData: values
+  })
+}
+
+describe('Photographer.vue', () => {
+  it('renders props when passed', () => {
+    const link = 'https://google.com';
+    const name = 'Minchan Jun';
+
+    const wrapper = factory({link, name});
+
+    expect(wrapper.props().link).toMatch(link);
+    expect(wrapper.props().name).toMatch(name);
+  })
+
+})
+
+// describe('Photographer.vue', () => {
+//   it('renders props when passed', () => {
+//     const link = 'https://google.com';
+//     const name = 'Minchan Jun'
+//     const wrapper = shallowMount(Photographer, {
+//       propsData: { link, name }
+//     })
+//     expect(wrapper.props().link).toMatch(link);
+//     expect(wrapper.props().name).toMatch(name);
+//   })
+
+// })
