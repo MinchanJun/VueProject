9b9fcb7e1efa7d8a735b19d7d5c7c5fe88b2372e
Minchan Jun <minchan.jun@capsim.com>
2020-11-25 09:57:58 -0500

    Practicing Life Cycle of Vue Created and Mounted with components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="app">
-    <Gallery v-bind:title="galleryTitle" /> 
+    <Gallery v-bind:title="galleryTitle" />
+    <h1>Glitch Text</h1>
+    <GlitchText :text="this.revealString" :speed="100" /><br>
+    <GlitchText :text="this.revealString" :speed="200" /><br>
+    <GlitchText :text="this.revealString" :speed="300" /><br>
+    <button @click="changeText">change the Text</button> 
   </div>
 </template>
 
 <script>
 import Gallery from './components/Gallery.vue'
+import GlitchText from './components/GlitchText.vue'
 
 export default {
   name: 'App',
   components: {
-    Gallery
+    Gallery,
+    GlitchText
   },
   data() {
     return {
-      galleryTitle: 'Photography Gallery'
+      galleryTitle: 'Photography Gallery',
+      revealString: 'New Jersey, United States, 1990'
+    }
+  },
+  methods: {
+    changeText() {
+      this.revealString = 'Seoul, Korea 1991'
     }
   }
 }
---
+++ src/components/GlitchText.vue
@@ -0,0 +1,82 @@
+<template>
+    <span>{{ currentText }} TimetoExecute:{{ currentSpeed }}</span> 
+</template>
+
+
+
+<script>
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+export default {
+    name: "GlitchText",
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        speed: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {
+            currentText: "",
+            defaultSpeed: 300, 
+            currentSpeed: 0,
+            glitchRange: 'abcdefghijklmnopqrstuvwxyz',
+            finalText: ""
+        };
+    },
+    watch: {
+        text: function(val) {
+            this.currentText = "";
+            this.displayText(val, "");
+        }
+    },
+    created() {
+        this.currentSpeed = this.speed;
+        // if (this.speed) {
+        //     this.currentSpeed = this.speed;
+        // } else {
+        //     this.currentSpeed = this.defaultSpeed;
+        // }
+    },
+    mounted() {
+        this.finalText = this.text;
+        this.displayText(this.text, "");
+    },
+    updated() {
+
+    },
+    methods: {
+        displayText(text, target) {
+            let self = this;
+            if (text.length > target.length + 1) {
+                setTimeout(() => {
+                    let newCurrent = target + text[target.length];
+                    newCurrent += self.glitchChar(self.glitchRange);
+                    target += text[target.length];
+                    self.currentText = newCurrent;
+                    self.displayText(text, target);
+                }, self.currentSpeed);
+
+            } else {
+                self.currentText = text;
+            }
+            // console.log(target);
+            // this.currentText = text;
+        },
+        glitchChar(characters) {
+            let char = characters[getRandomInt(characters.length -1)];
+            return char;
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
